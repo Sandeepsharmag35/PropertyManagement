92dbd59408df614eaae905ffd307e0c435d42ec3
Sandeepsharmag35 <72307548+Sandeepsharmag35@users.noreply.github.com>
2022-04-18 21:31:22 +0545

    added new files

---
+++ templates/login.php
@@ -0,0 +1,33 @@
+<?php
+include_once('header.php');
+
+
+?>
+<div class="loginback">
+    <div class=" logindiv">
+        <form class="login-form">
+            <h1>
+                <center>Login</center>
+            </h1>
+            <div class="form-input">
+                <label class="form-label" for="username">Username:</label>
+                <input class="form-control" type="text" id="username" placeholder="Enter Username">
+            </div>
+            <div class="form-input">
+
+                <label class="form-label" for="password">Password:</label>
+                <input class="form-control" type="Password" id="password" placeholder="Enter Password">
+
+            </div>
+            <div class="form-input">
+                <center><button type="submit">Login</button></center>
+            </div>
+            <div class="form-input">
+                <center>Forgot Password?</center>
+        </form>
+
+
+
+    </div>
+
+</div>
